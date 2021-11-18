175c65d7f7b4684a8610f426f25bbb16ec00471d
nikita <nikita03565@gmail.com>
2021-11-18 17:21:56 +0300

    upd headers

--- scraper/utils.py
+++ scraper/utils.py
@@ -6,18 +6,19 @@ from lxml.html import fromstring, tostring
 default_headers = {
     "authority": "pikabu.ru",
     "cache-control": "max-age=0",
-    "sec-ch-ua": '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+    "sec-ch-ua": '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
     "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Linux"',
     "dnt": "1",
     "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "sec-fetch-site": "same-origin",
+    "sec-fetch-site": "none",
     "sec-fetch-mode": "navigate",
     "sec-fetch-user": "?1",
     "sec-fetch-dest": "document",
-    "accept-language": "en-US,en;q=0.9,ru;q=0.8,ru-RU;q=0.7,da;q=0.6",
-    "cookie": "pcid=jYDleGx8vv2; sf=0; zukoVisitorId=YvEe9bnKqRxeJoW6qSv9DS94ATChNCna; _ym_uid=1614254812820875768; _ym_d=1614254812; _ga=GA1.2.517054697.1614254813; tmr_lvid=4065e0635307cc9e3cffabb261d4f554; tmr_lvidTS=1614546674099; tmr_reqNum=5; _gid=GA1.2.1428891017.1615125979; PHPSESS=6hol1gvfpuaanffasvdpadol4n; _ym_isad=1; fps=d0b49c942cdf5e18cbd7d82a83c5f6cd1a; is_scrollmode=1; ulfs=1615230745; pin_8064729=1; pkb_modern=11; k2s7=K-M:B:DEjC; la=1615231028_3948_4219_4219__; bs=F1; vn_buff=[8065360]; la=1615232056_3948_4219_4219__; pcid=jYDleGx8vv2; sf=0",
+    "accept-language": "en-GB,en;q=0.9",
+    "cookie": "PHPSESS=8ebfqvk63ob4mse3h9m0la531v; is_scrollmode=1; pcid=jRUlRUGbsv2; grex2=%7B%22sidebar_adv_20210714%22%3A%22gr1%22%7D; la=1637245206_10334__10334__; clpr1=Aw.AA.AAA.AAAAAAAAAAA; pkb_modern=11; zukoVisitorId=UW4oIamRtVTDyfPX0eQTC8PxtwdeHn9y; bs=E0; _ym_uid=163724520823638306; _ym_d=1637245208; _ga=GA1.2.942804580.1637245208; _gid=GA1.2.803218438.1637245208; _gat_gtag_UA_28292940_1=1; _ym_isad=2; _fbp=fb.1.1637245208333.1684597788; ulfs=1637245208; sf=0; cto_bundle=54Voe19aTnJNU2I3VnZuVWVkOTFzaXNXSSUyRnZ2V0FUUENjdHdQUDUzMjFtbXYxQ25DQ3lsY0lKVTJMb1JDV050YzY5Y01mWllHSG52Y3F1aHNPTlExMWpiTyUyRnQlMkZZJTJCZDVzSXBKYk5OMHY2JTJCYlhwR1ZaS01EOVliTjNmWFdNMkZvak93MnlFcEVtYk9sUm12NmZIRjBLNGlFRnNnJTNEJTNE; __gads=ID=eebb38a1773e228d:T=1637245208:S=ALNI_MZfOa6a4on7reERkIVXeoTnoVrClg; vn_buff=[8624411]; grex2=%7B%22sidebar_adv_20210714%22%3A%22gr1%22%7D; la=1637245206_10334__10334__; pcid=jGMlmZXQtv2; sf=0; ulfs=1637245244",
 }
 
 
