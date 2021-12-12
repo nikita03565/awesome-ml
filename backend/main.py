ea4871ff5cd7ff65c6c3116ace7058881ab2cccf
nikita <nikita03565@gmail.com>
2021-12-12 14:35:49 +0300

    use fastapi's template rendering

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
-from api.predictions.predictions_api import router
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from api.predictions.predictions_api import router
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="templates"), name="static")
+templates = Jinja2Templates(directory="templates/html")
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,9 +21,12 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
