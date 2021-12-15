92a2090dcec4b6f33e62cce15fcbfa98d413a8f6
nikita <nikita03565@gmail.com>
2021-12-15 23:49:15 +0300

    clean up. and move constants up

--- predictor/predict.py
+++ predictor/predict.py
@@ -1,19 +1,19 @@
+import json
 import os
 
 import mlflow
-import yaml
-import json
 import pandas as pd
-
+import yaml
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from predictor.utils import stem, get_version_model, get_production_model_version
+
+from predictor.utils import stem, get_production_model_version
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 config_path = os.path.join(parent_dir, "predictor", "config.yaml")
 vocabulary_path = os.path.join(parent_dir, "predictor", "vocabulary.json")
 
-config_path = config = yaml.safe_load(open(config_path))
+config_path = yaml.safe_load(open(config_path))
 global_config = config_path["global"]
 config = config_path["train"]
 predict_config = config_path["predict"]
@@ -21,35 +21,27 @@ predict_config = config_path["predict"]
 count_vect_params = config["count_vect_params"]
 catboost_params = config["catboost_params"]
 
+with open(vocabulary_path, "r") as f:
+    vocabulary = json.loads(f.read())
+
+vectorizer = CountVectorizer(vocabulary=vocabulary, max_features=count_vect_params['max_features'])
+
+mlflow.set_tracking_uri(global_config["mlflow_uri"])
+latest_version = get_production_model_version(config["model_name"]) or predict_config["version"]
+loaded_model = mlflow.pyfunc.load_model(model_uri=predict_config["model_path"])
+
+
 def get_prediction(article_text: str):
     stem_text = pd.Series(data=stem(article_text))
     tfidfconverter = TfidfTransformer()
-    with open(vocabulary_path, "r") as f:
-        vocabulary = json.loads(f.read())
 
-        vectorizer = CountVectorizer(vocabulary=vocabulary, max_features=count_vect_params['max_features'])
-        X_countVectorizer = vectorizer.fit_transform(stem_text).toarray()
-        X_tfIdf = tfidfconverter.fit_transform(X_countVectorizer)
-        
-        mlflow.set_tracking_uri(global_config["mlflow_uri"])
+    X_countVectorizer = vectorizer.fit_transform(stem_text).toarray()
+    X_tfIdf = tfidfconverter.fit_transform(X_countVectorizer)
 
-        model_name = config["model_name"]
-        latest_version = get_production_model_version(config["model_name"]) or predict_config["version"]
+    test_predictions = loaded_model.predict(X_tfIdf)
 
-        print("set_tracking_uri ", global_config["mlflow_uri"])
-        print("model_name ", model_name)
-        print("latest_version ", latest_version)
-    
-        # не хочет работать 
-        # loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{latest_version}")
+    return test_predictions[0]
 
-        # работает
-        # loaded_model = mlflow.pyfunc.load_model(model_uri=os.path.join(parent_dir, "mlflow", "1", "1d847a48016e4392943c6cb48e0418ab", "artifacts", "regression"))
-
-        loaded_model = mlflow.pyfunc.load_model(model_uri=predict_config["model_path"])
-        test_predictions = loaded_model.predict(X_tfIdf)
-
-        return test_predictions[0]
 
 if __name__ == "__main__":
     print(get_prediction("Вот тогда мы и использовали стикеры, чтоб не дай боже глянуть. Кстати, она оказалась, в отличие от обычных высокомерных "))
