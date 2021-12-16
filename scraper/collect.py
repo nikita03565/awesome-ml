41416e439f588d279585360dad79b9e612407093
nikita <nikita03565@gmail.com>
2021-12-16 11:51:38 +0300

    updated scraper logic

--- scraper/collect.py
+++ scraper/collect.py
@@ -1,30 +1,33 @@
+import datetime
 import json
 import os
 import time
 from pathlib import Path
 
 import requests
+import yaml
 
 from scraper.utils import get_logger, default_headers, extract_articles
-
 
 logger = get_logger(__name__)
 
 # In days query parameters 943 stands for 1 Aug 2010 and 4815 -- 8 March 2021
 search_url = "https://pikabu.ru/search?d={day_start}&D={day_end}&page={page}"
 
-# Long stories (aka Длиннопост)
+# Long stories
 search_with_tag_url = "https://pikabu.ru/tag/%D0%94%D0%BB%D0%B8%D0%BD%D0%BD%D0%BE%D0%BF%D0%BE%D1%81%D1%82?d={day_start}&D={day_end}&page={page}"
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 output_dir = os.path.join(parent_dir, "data", "json")
+
+config_path = os.path.join(parent_dir, "scraper", "config.yaml")
+config = yaml.safe_load(open(config_path))
+collect_config = config["collect"]
 file_name = "{start}_{end}_{page}.json"
 days_step = 1
 earliest_date = 943
 
-
-
-max_iterations = 10
+default_start = (datetime.date.today() - datetime.date(2010, 8, 1)).days + 943
 
 
 def start_scraper():
@@ -32,14 +35,17 @@ def start_scraper():
     This function requests html page from a given url in a loop,
     extracts content of <article> tag and saves result as a list in json file.
     """
-    global n_iterations
+    max_iterations = int(collect_config.get("max_iterations", 10))
     Path(output_dir).mkdir(exist_ok=True)
     session = requests.session()
     session.headers.update(default_headers)
-    # We want to continue from the date that we have stopped on. Find the least start date from existing files for that
+
+    countdown_start = int(collect_config.get("countdown_start", 0)) or default_start
+
+    # We might want to continue from the date that we have stopped on.
+    # Find the least start date from existing files for that
     files = sorted(os.listdir(output_dir))
-    countdown_start = 5000  # 9 September 2021
-    if files:
+    if files and collect_config.get("continue_from_files"):
         countdown_start = int(files[0].split("_")[0])
 
     start = countdown_start - days_step
@@ -48,7 +54,7 @@ def start_scraper():
     while start > earliest_date:
         logger.debug(f"Searching in {start} - {end} range")
         for page in range(1, 100):
-            if n_iterations > max_iterations:
+            if max_iterations and n_iterations > max_iterations:
                 # THIS WAS MADE FOR TESTING PURPOSES ONLY
                 raise SystemExit
             n_iterations += 1
